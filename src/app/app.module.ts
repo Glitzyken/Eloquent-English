aeb09c5b960fd25913a7f135f004fa10ef0ec7fb
Kenneth Jimmy <kenjimmy17@gmail.com>
2019-12-31 19:48:47 +0100

    removed cta-button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
-import { HomePageComponent } from './home-page/home-page.component';
-import { TopNavComponent } from './top-nav/top-nav.component';
-import { CtaButtonComponent } from './cta-button/cta-button.component';
+import { AppComponent } from "./app.component";
+import { HomePageComponent } from "./home-page/home-page.component";
+import { TopNavComponent } from "./top-nav/top-nav.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomePageComponent,
-    TopNavComponent,
-    CtaButtonComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, HomePageComponent, TopNavComponent],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
