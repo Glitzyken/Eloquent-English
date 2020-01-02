721ea86dbbf0c54e656f44b803fff8916fad777d
Kenneth Jimmy <kenjimmy17@gmail.com>
2020-01-02 19:07:03 +0100

    add bg color to top-nav

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./home-page.component.scss"]
 })
 export class HomePageComponent implements OnInit {
+  ipaUrlKindle = "https://www.amazon.co.uk/dp/B07RZQ5Q2D";
   constructor() {}
 
   ngOnInit() {}
--- src/app/top-nav/top-nav.component.ts
+++ src/app/top-nav/top-nav.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-top-nav',
-  templateUrl: './top-nav.component.html',
-  styleUrls: ['./top-nav.component.scss']
+  selector: "app-top-nav",
+  templateUrl: "./top-nav.component.html",
+  styleUrls: ["./top-nav.component.scss"]
 })
 export class TopNavComponent implements OnInit {
+  eloquentUrlKindle: "https://www.amazon.com/dp/B0831PZ4VM";
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
