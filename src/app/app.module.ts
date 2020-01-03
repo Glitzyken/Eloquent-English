c5200a0042f8e7631984510f9be72cb19b6a8d6d
Kenneth Jimmy <kenjimmy17@gmail.com>
2020-01-03 23:32:32 +0100

    add side-nave component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { HomePageComponent } from "./home-page/home-page.component";
 import { TopNavComponent } from "./top-nav/top-nav.component";
+import { SideNavComponent } from './side-nav/side-nav.component';
 
 @NgModule({
-  declarations: [AppComponent, HomePageComponent, TopNavComponent],
+  declarations: [AppComponent, HomePageComponent, TopNavComponent, SideNavComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
