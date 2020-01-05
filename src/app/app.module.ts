2de3684da6162b63b85da1583530c2cd2435b05b
Kenneth Jimmy <kenjimmy17@gmail.com>
2020-01-05 22:22:22 +0100

    remove contents component and make chapter components reusable

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,17 @@ import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { HomePageComponent } from "./home-page/home-page.component";
 import { TopNavComponent } from "./top-nav/top-nav.component";
-import { ContentsComponent } from "./contents/contents.component";
 import { Chapter1Component } from "./chapter1/chapter1.component";
-import { NextButtonComponent } from './next-button/next-button.component';
-import { PrevButtonComponent } from './prev-button/prev-button.component';
-import { Chapter2Component } from './chapter2/chapter2.component';
-import { SideNavComponent } from './side-nav/side-nav.component';
+import { NextButtonComponent } from "./next-button/next-button.component";
+import { PrevButtonComponent } from "./prev-button/prev-button.component";
+import { Chapter2Component } from "./chapter2/chapter2.component";
+import { SideNavComponent } from "./side-nav/side-nav.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
     TopNavComponent,
-    ContentsComponent,
     Chapter1Component,
     NextButtonComponent,
     PrevButtonComponent,
