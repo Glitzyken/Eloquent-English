de34ed6b885fe7840e2bb5322ae832424b3fd494
Kenneth Jimmy <kenjimmy17@gmail.com>
2020-01-20 21:23:35 +0100

    add not found 404 component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,23 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
-import { AppComponent } from "./app.component";
-import { HomePageComponent } from "./home-page/home-page.component";
-import { TopNavComponent } from "./top-nav/top-nav.component";
-import { Chapter1Component } from "./chapter1/chapter1.component";
-import { NextButtonComponent } from "./next-button/next-button.component";
-import { PrevButtonComponent } from "./prev-button/prev-button.component";
-import { Chapter2Component } from "./chapter2/chapter2.component";
-import { SideNavComponent } from "./side-nav/side-nav.component";
+import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { TopNavComponent } from './top-nav/top-nav.component';
+import { Chapter1Component } from './chapter1/chapter1.component';
+import { NextButtonComponent } from './next-button/next-button.component';
+import { PrevButtonComponent } from './prev-button/prev-button.component';
+import { Chapter2Component } from './chapter2/chapter2.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
 import { Chapter3Component } from './chapter3/chapter3.component';
 import { Chapter4Component } from './chapter4/chapter4.component';
 import { Chapter5Component } from './chapter5/chapter5.component';
 import { Chapter6Component } from './chapter6/chapter6.component';
 import { FooterComponent } from './footer/footer.component';
 import { ButtonNavComponent } from './button-nav/button-nav.component';
+import { ChaptersComponent } from './chapters/chapters.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
@@ -31,9 +34,24 @@ import { ButtonNavComponent } from './button-nav/button-nav.component';
     Chapter5Component,
     Chapter6Component,
     FooterComponent,
-    ButtonNavComponent
+    ButtonNavComponent,
+    ChaptersComponent,
+    NotFoundComponent
   ],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: '', component: HomePageComponent },
+      { path: 'chapters', component: ChaptersComponent },
+      { path: 'chapter1', component: Chapter1Component },
+      { path: 'chapter2', component: Chapter2Component },
+      { path: 'chapter3', component: Chapter3Component },
+      { path: 'chapter4', component: Chapter4Component },
+      { path: 'chapter5', component: Chapter5Component },
+      { path: 'chapter6', component: Chapter6Component },
+      { path: '**', component: NotFoundComponent }
+    ])
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
